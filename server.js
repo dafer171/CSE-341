c2e3873878c2661abb8c39f09f5bf8c217e5767a
David <davidp@ntestinc.com>
2022-09-13 20:07:59 +0200

    Conection with MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port =  3000; //process.env.PORT ||
+const mongodb = require('./db/connect')
+const port =  process.env.PORT || 3000; 
 
 app.use('/', require('./routes'))
 /*
@@ -8,6 +9,17 @@ app.get('/',(req, res) => {
     res.send('Hello World!')
 })
 */
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log('Connected to DB and listening on ${port}')
+    }
+});
+/*
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
 })
+*/
