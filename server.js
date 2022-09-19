96a13cc5b6f85195c841900e3c227922b552b74d
David <davidp@ntestinc.com>
2022-09-19 13:32:36 +0200

    render

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const app = express();
-const mongodb = require('./db/connect')
-const port =  process.env.PORT || 3000; 
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
 
-app.use('/', require('./routes'))
-/*
-app.get('/',(req, res) => {
-    res.send('Hello World!')
-})
-*/
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  })
+  .use('/contacts', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(port);
-        console.log(`Connected to DB and listening on ${port}`)
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
 });
-/*
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
-})
-*/
