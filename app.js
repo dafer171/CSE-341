4a29b3e6a104f4af342924be2d7e3bd5b90ca42a
David <davidp@ntestinc.com>
2022-09-19 13:26:52 +0200

    render commit 2

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  })
+  .use('/contacts', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,29 @@
 displayName = (req, res) => {
-    const name = "David Fernando Peña";
-    res.status(200).send(name);
+    const names = {
+        "contact" : {
+            "fistName": "David",
+            "LastName": "Peña",
+            "email": "daf_171@hotmail.com",
+            "favoriteColor": "Red",
+            "birthday": "17/10/1988"
+        },
+        "contact2" : {
+            "fistName": "Azahara",
+            "LastName": "Soler",
+            "email": "daf_171@hotmail.com",
+            "favoriteColor": "Pink",
+            "birthday": "30/12/1991"
+        },
+        "contact3" : {
+            "fistName": "Jenny",
+            "LastName": "Andrade",
+            "email": "daf_171@hotmail.com",
+            "favoriteColor": "Green",
+            "birthday": "27/02/1966"
+        }
+    }
+    ;
+    res.status(200).send(names);
 };
 
 module.exports = {
