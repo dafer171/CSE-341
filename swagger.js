abbca63ad3ad80687ea877d927c3d9f93c0a139c
David <davidp@ntestinc.com>
2022-09-24 21:13:10 +0200

    render contacts api ready

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     desciption: 'Contact API'
   },
-  host: 'https://web-service-cse-341.onrender.com',
-  schemes: ['http']
+  host: 'web-service-cse-341.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
