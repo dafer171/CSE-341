a58288a1cb3f5f68e981df2466ab659f04cef641
David <davidp@ntestinc.com>
2022-09-24 22:03:03 +0200

    Corregido swagger.js

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,7 @@
-const router = require('express').Router;
+const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-const { route } = require('./contacts');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-module.export = router;
+module.exports = router;
